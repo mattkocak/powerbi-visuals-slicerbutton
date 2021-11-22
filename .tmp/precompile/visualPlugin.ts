d494338e5058bb95ed693bb07760a40fb887b57f
Matt Kocak <kocak_cc25@hotmail.com>
2021-11-22 11:24:13 +0100

    Initial PBI files

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,32 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
+import DialogConstructorOptions = powerbiVisualsApi.extensibility.visual.DialogConstructorOptions;
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+var filterButton1A50067195CD4C50B9963487F1787C7C_DEBUG: IVisualPlugin = {
+    name: 'filterButton1A50067195CD4C50B9963487F1787C7C_DEBUG',
+    displayName: 'FilterButton',
+    class: 'Visual',
+    apiVersion: '3.8.0',
+    create: (options: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+        throw 'Visual instance not found';
+    },
+    createModalDialog: (dialogId: string, options: DialogConstructorOptions, initialState: object) => {
+        const dialogRegistry = globalThis.dialogRegistry;
+        if (dialogId in dialogRegistry) {
+            new dialogRegistry[dialogId](options, initialState);
+        }
+    },
+    custom: true
+};
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["filterButton1A50067195CD4C50B9963487F1787C7C_DEBUG"] = filterButton1A50067195CD4C50B9963487F1787C7C_DEBUG;
+}
+export default filterButton1A50067195CD4C50B9963487F1787C7C_DEBUG;
